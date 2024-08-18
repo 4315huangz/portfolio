514fa835b68574ba4a3dfef48bf1160d6f74577a
Natalie <ziweih1993@gmail.com>
2024-08-18 17:19:12 -0400

    Google sheet app script test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,51 @@
 //--------------------header and footer----------------------
 function loadHTML(selector, file) {
     fetch(file)
-    .then(response => response.text())
-    .then(data => document.querySelector(selector).innerHTML = data)
-    .catch(error => console.error("Error loading HTML: ", error));
+        .then(response => response.text())
+        .then(data => document.querySelector(selector).innerHTML = data)
+        .catch(error => console.error("Error loading HTML: ", error));
 }
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     loadHTML("#header", "header.html");
     loadHTML("#footer", "footer.html");
 });
 
 //--------------------resume---------------------------------
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const textElement = document.getElementById('typing-text');
     const text = textElement.textContent;
-    textElement.textContent = ''; 
+    textElement.textContent = '';
     let index = 0;
 
     function type() {
         if (index < text.length) {
             textElement.textContent += text.charAt(index);
             index++;
-            setTimeout(type, 100); 
+            setTimeout(type, 100);
         }
     }
     type();
 });
 
 //--------------------pet---------------------------------
-document.getElementById("scrollButton").addEventListener("click", function() {
+document.getElementById("scrollButton").addEventListener("click", function () {
     document.getElementById("about").scrollIntoView({ behavior: 'smooth' });
 });
 
 //--------------------message---------------------------------
-const spreedsheetURL = "https://script.google.com/macros/s/AKfycbx6WqH-y4zYd5f_XOQyPHu2pVTgKJZHDfkMz_wNhwHBf8O_pZc_Cs3t49MXFLHi6DMx/exec";
-const form = document.forms["submit-to-google-sheet"];
+const scriptURL = "https://script.google.com/macros/s/AKfycbxbdjSwQmeu8LLWuTg8PVD5PZB0sAvKsCTu5NgxNTJPvhe_o1_I0K0uZ2kifT_xW4kO/exec";
+const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById("msg");
 
-form.addEventListener("submit", e => {
-    e.preventDefault(); 
-    console.log("Form submission started.")
-    fetch(spreedsheetURL, { method: "POST", body: new FormData(form)})
-    .then(response => {
-        console.log("Form submission response:", response);
-        msg.innerHTML = "Message sent successfully!"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        }, 5000)
-        form.reset()
-    })
-    .catch(error => {
-        console.error("Error!", error.message);
-    });
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully!";
+            setTimeout(function() {
+                msg.innerHTML = "";
+            }, 5000)
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message));
 })
