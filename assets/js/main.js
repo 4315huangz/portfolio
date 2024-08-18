2e34553ac162c0aeb28ea17ccdbbd71eeaf5a4b5
Natalie <ziweih1993@gmail.com>
2024-08-18 15:48:40 -0400

    Add header.html and footer.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+//--------------------header and footer----------------------
+function loadHTML(selector, file) {
+    fetch(file)
+    .then(response => response.text())
+    .then(data => document.querySelector(selector).innerHTML = data)
+    .catch(error => console.error("Error loading HTML: ", error));
+}
+document.addEventListener("DOMContentLoaded", function() {
+    loadHTML("#header", "header.html");
+    loadHTML("#footer", "footer.html");
+});
+
 //--------------------resume---------------------------------
 document.addEventListener('DOMContentLoaded', function() {
     const textElement = document.getElementById('typing-text');
