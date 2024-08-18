8c88720850337635bce50d4b4821e3bbf610827f
Natalie <ziweih1993@gmail.com>
2024-08-18 15:19:15 -0400

    Connect to google sheet api for message page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//--------------------resume---------------------------------
 document.addEventListener('DOMContentLoaded', function() {
     const textElement = document.getElementById('typing-text');
     const text = textElement.textContent;
@@ -14,6 +15,29 @@ document.addEventListener('DOMContentLoaded', function() {
     type();
 });
 
+//--------------------pet---------------------------------
 document.getElementById("scrollButton").addEventListener("click", function() {
     document.getElementById("about").scrollIntoView({ behavior: 'smooth' });
 });
+
+//--------------------message---------------------------------
+const spreedsheetURL = "https://script.google.com/macros/s/AKfycbx6WqH-y4zYd5f_XOQyPHu2pVTgKJZHDfkMz_wNhwHBf8O_pZc_Cs3t49MXFLHi6DMx/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", e => {
+    e.preventDefault(); 
+    console.log("Form submission started.")
+    fetch(spreedsheetURL, { method: "POST", body: new FormData(form)})
+    .then(response => {
+        console.log("Form submission response:", response);
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+    })
+    .catch(error => {
+        console.error("Error!", error.message);
+    });
+})
